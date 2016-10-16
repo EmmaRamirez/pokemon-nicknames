ebc2c830d97353ed2d066d1f00c274e434aabacb
Emma Ramirez <ramirez.emma.g@gmial.com>
2016-10-16 06:21:44 -0600

    Switch limit from Index to Root Component

--- public/components/Root.tsx
+++ public/components/Root.tsx
@@ -7,6 +7,7 @@ import Footer from './Footer';
 
 interface RootProps {
   data: any[];
+  limit: number;
 }
 
 
@@ -15,12 +16,15 @@ class Root extends React.Component<RootProps, {}> {
     super(props);
   }
   render() {
+    let limit = this.props.limit;
     let pokemonComponents = this.props.data.map(function (item, index) {
-      return <PokemonNickname
-              pokemon={item.pokemon}
-              nickname={item.nickname}
-              key={index}
-             />
+      if (index < limit) {
+        return <PokemonNickname
+                pokemon={item.pokemon}
+                nickname={item.nickname}
+                key={index}
+               />
+      }
     });
     return (
       <div>
