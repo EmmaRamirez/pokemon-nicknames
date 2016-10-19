42edc14d2b5002a3d62dfaf6c344275f313a3c78
Emma Ramirez <ramirez.emma.g@gmial.com>
2016-10-19 00:03:49 -0600

    fix favorites

--- public/components/Header.tsx
+++ public/components/Header.tsx
@@ -17,7 +17,9 @@ class Header extends React.Component<{}, {}> {
       <header className='app-header'>
         <div className='app-header-inner'>
           <h1><a href='/'><img height='24px' src='../img/pokeball.svg' /> Pokemon Nicknames</a></h1>
-          <a href='/favorites'>Favorites</a>
+          {/*
+            }<a href='/favorites'>Favorites</a>
+          */}
           <a href='/submit-nickname'>Submit Nickname</a>
           {/*
             <a href='/random'><i className='fa fa-random'></i> Random</a>
