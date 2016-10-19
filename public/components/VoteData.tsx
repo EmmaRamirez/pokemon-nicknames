ac81e0d22aeecf69ad8cc8db23b2b385ea2a9b21
Emma Ramirez <ramirez.emma.g@gmial.com>
2016-10-19 09:34:34 -0600

    remove ref from notification

--- public/components/VoteData.tsx
+++ public/components/VoteData.tsx
@@ -4,7 +4,6 @@ import { Router, Route, Link, browserHistory } from 'react-router';
 import * as _ from 'underscore';
 import Pokemon from '../interfaces/Pokemon';
 import Nickname from '../interfaces/Nickname';
-import Notification from './Notification';
 
 interface VoteDataProps {
   pokemon: Pokemon;
