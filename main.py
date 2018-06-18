49b4ebeb77b679997862403c209d30ad4b8201f4
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-06-17 22:02:04 -0600

    fix: paginate results

--- main.py
+++ main.py
@@ -92,6 +92,10 @@ class PokemonHandler(web.RequestHandler):
         document = db.pokemon.find().sort("id").limit(30)
         return document
 
+    def post(self):
+        db.pokemon.save()
+        return 'It worked'
+
     def get(self, slug=None):
         logger.info('requested {}'.format(slug))
         entry = None
@@ -103,12 +107,33 @@ class PokemonHandler(web.RequestHandler):
             d = self.find_30()
         self.write(dumps(d))
 
+class PokemonPageHandler(web.RequestHandler):
+    def set_default_headers(self):
+        logger.info('Setting headers...')
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
+    def retrieve(self, page=1, limit=30):
+        skipN = (page - 1) * limit
+        document = db.pokemon.find().sort("id").skip(skipN).limit(limit)
+        return document
+
+    def get(self, slug=1):
+        logger.info('retrieving page {}'.format(slug))
+        content = None
+        if is_number(slug):
+            logger.info(slug)
+            content = self.retrieve(int(slug))
+        self.write(dumps(content))
+
 
 def make_app():
     return web.Application([
         (r"/", MainHandler),
         (r"/pokemon", PokemonHandler),
         (r"/pokemon/([^/]+)", PokemonHandler),
+        (r"/pokemon/page/([^/]+)", PokemonPageHandler),
         (r"/vote", VoteHandler),
         (r"/submit-nickname", NicknameHandler)
     ], db=db)
