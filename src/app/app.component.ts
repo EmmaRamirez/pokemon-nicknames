33000279fe04ea3326939b28b1d14828c53ab2a5
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-06-24 14:07:05 -0600

    design: switch to dark gray theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Pokemon } from './pokemon';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
